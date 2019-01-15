8b40d2bf4a7e8bf3a0af8ba8999d009458684fde
admin <you@example.com>
2019-01-14 22:20:49 -0600

    Actualizar informacion

---
+++ PutDescripcion.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "servicio1",
+  password: "servicio1.123",
+  database: "Mobasign"
+});
+
+connection.connect();
+
+
+app.put('/rest/descripcion', function (req, res) {
+   connection.query('UPDATE `Descripcion` SET `Descripcion`=?, `Id_Articulo`=? ,`Estatus`=? where `Id_Descripcion`=?', [req.body.Descripcion,req.body.Id_Articulo,req.body.Estatus,req.body.Id_Descripcion],function (error, results, fields) {
+	  if (error) throw error;
+	  res.end(JSON.stringify(results));
+	});
+});
+
+
+app.listen(2468, function () {
+ console.log('Node app is running on port 2468');
+
+});  
+
+
+
+
